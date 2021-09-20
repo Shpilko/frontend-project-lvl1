d26637712f0613ca887f7ff859b62c32b8b1bec5
Evgeniy Shpilko <callelia@yandex.ru>
2021-09-20 22:14:07 +0300

    add cli.js, add package-lock.json

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,10 @@
 #!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+
 console.log('Welcome to the Brain Games!');
+console.log('May I have your name?');
+
+const name = readlineSync.question();
+
+console.log(`Hello, ${name}!`);
