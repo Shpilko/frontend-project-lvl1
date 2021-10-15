c5aa389ea38d47a2b2488247050b04cc3fbd09ef
Evgeniy Shpilko <callelia@yandex.ru>
2021-10-15 11:34:36 +0300

    changed bin/brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -3,36 +3,3 @@
 import brainEven from '../src/games/even.js';
 
 brainEven();
-
-// import readlineSync from 'readline-sync';
-// import { getRandomInt } from '../src/getRandomInt.js';
-
-// console.log('Welcome to the Brain Games!');
-
-// const name = readlineSync.question('May I have your name? ');
-
-// console.log(`Hello, ${name}!`);
-// console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-// for (let raundCount = 1; raundCount <= 3; raundCount += 1) {
-//   const randomInt = getRandomInt(1, 100);
-//   console.log(`Question: ${randomInt}`);
-
-//   const evenNum = (randomInt % 2 === 0) ? 'yes' : 'no';
-
-//   const userAnswer = readlineSync.question('Your answer: ');
-//   const correctAnswer = evenNum;
-
-//   if (userAnswer === correctAnswer) {
-//     console.log('Correct!');
-//   }
-//   if (raundCount === 3) {
-//     console.log(`Congratulations, ${name}!`);
-//     break;
-//   }
-//   if (userAnswer !== correctAnswer) {
-//     console.log(`'${userAnswer}' is a wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
-
-//     break;
-//   }
-// }
