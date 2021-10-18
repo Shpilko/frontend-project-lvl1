a0613f2132e7b4c9e7595d9d83e0b02b691333c7
Evgeniy Shpilko <callelia@yandex.ru>
2021-10-18 22:45:34 +0300

    changed

--- src/games/even.js
+++ src/games/even.js
@@ -3,11 +3,12 @@ import newGame from '../index.js';
 
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
+const isEven = (number) => (number % 2 === 0);
+
 const getQuestionAndCorrectAnswer = () => {
   const randomInt = getRandomInt(1, 100);
-  const evenNum = (randomInt % 2 === 0) ? 'yes' : 'no';
   const question = randomInt;
-  const correctAnswer = evenNum;
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
 
   return [question, correctAnswer];
 };
