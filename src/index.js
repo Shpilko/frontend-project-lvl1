d8ef8c24d2d5787955e81e6fb3b83f571d9105b1
Evgeniy Shpilko <callelia@yandex.ru>
2021-10-18 16:10:29 +0300

    add guard expression to nesGame

--- src/index.js
+++ src/index.js
@@ -14,13 +14,15 @@ const newGame = (description, getQuestionAndCorrectAnswer) => {
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
 
-    if (userAnswer === correctAnswer) {
-      console.log('Correct!');
-    } else {
+    if (userAnswer !== correctAnswer) {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${name}!`);
 
       return;
+    }
+
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
     }
   }
 
