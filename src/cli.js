616e396a09041cac695bcc0e639eb6d7e9396c4e
Evgeniy Shpilko <callelia@yandex.ru>
2021-10-18 12:41:37 +0300

    add greeting function

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,5 @@
 const readlineSync = require('readline-sync');
 
-// const userName = readlineSync.question('May I have your name? ');
-// console.log(`Hello, ${userName}!`);
-
-// export default userName;
-
 const greeting = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
