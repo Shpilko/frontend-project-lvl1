52038d40b22213448319c4061d9748abdf5f227a
Evgeniy Shpilko <callelia@yandex.ru>
2021-10-22 01:42:57 +0300

    add boolean result

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,22 +5,22 @@ const description = 'Answer "yes" if given number is prime. Otherwise answer "no
 
 const isPrime = (num) => {
   if (num < 2) {
-    return 'no';
+    return false;
   }
 
   for (let i = 2; i <= Math.sqrt(num); i += 1) {
     if (num % i === 0) {
-      return 'no';
+      return false;
     }
   }
 
-  return 'yes';
+  return true;
 };
 
 const getQuestionAndCorrectAnswer = () => {
   const randomInt = getRandomInt(1, 101);
   const question = `${randomInt}`;
-  const correctAnswer = isPrime(randomInt);
+  const correctAnswer = isPrime(randomInt) ? 'yes' : 'no';
 
   return [question, correctAnswer];
 };
