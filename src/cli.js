09ca793436918c182c5044ed0be8b1c2cf092ac3
Evgeniy Shpilko <callelia@yandex.ru>
2021-10-22 00:41:32 +0300

    changed cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,6 @@
 const readlineSync = require('readline-sync');
 
 const welcome = () => {
-  console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
